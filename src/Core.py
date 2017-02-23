df6e781044bf6beab09be228945f1785ec91287c
Tookie <david.galy@epitech.eu>
2017-02-23 22:55:02 +0100

    add trending output

---
+++ src/Core.py
@@ -0,0 +1,8 @@
+class Core:
+
+    def __init__(self, nbVideo, nbEndPoints, nbRequests, nbServers, capacityServer):
+        self.nbVideo = nbVideo
+        self.nbEndPoints = nbEndPoints
+        self.nbRequests = nbRequests
+        self.nbServers = nbServers
+        self.capacityServer = self.capacityServer
